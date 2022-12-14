876dcd2302df22269b11c776c52ed1299770c35a
lucianafatino <lucianavenancio54@gmail.com>
2022-12-14 11:32:37 -0300

    Project 01

---
+++ Interactive Ninja Quiz/app.js
@@ -0,0 +1,34 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const respostasUser = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    // check answers
+    respostasUser.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 25;
+        }
+    });
+    
+    // show result on page
+    scrollTo(0,0);
+    result.classList.remove('d-none');
+
+    // score animation
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        } else{
+            output++;
+        }
+    }, 10);
+}); 
+
+
---
+++ Interactive Ninja Quiz/notes.md
@@ -0,0 +1,14 @@
+# Project - Interactive Ninja Quiz
+
+- Esse capítulo cobriu uma boa parte dos assuntos de DOM que estudamos no módulo anterior, assim como callback functions e temas simples como if/else. 
+
+- O projeto foi a criação de um quiz simples, com animação na pontuação e utilização básica do Bootstrap. Os assuntos tratados foram:
+    - Callback functions
+    - forEach
+    - if/else
+    - Eventos DOM
+        - querySelector
+        - addEventListener
+        - scrollTo
+        - setInterval
+        - etc
